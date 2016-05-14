bb6b092d922b943964db93f4d3466fec503d624a
Nguyễn Thắng <nguyenthang@Nguyens-MacBook-Pro.local>
2016-05-15 01:10:19 +0700

    #2.2.1 fix small styles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,22 @@
 class CommentsController < ApplicationController
+	before_action :find_book
 
 	def create
-		@book = Book.find(params[:book_id])
 		@comment = @book.comments.create(comment_params)
 		redirect_to book_path(@book)
 	end
 
 	def destroy
-		@book = Book.find(params[:book_id])
 		@comment = @book.comments.find(params[:id])
 		@comment.destroy
 		redirect_to book_path(@book)
 	end
+
 	private
+
+	def find_book
+		@book = Book.find(params[:book_id])
+	end
 
 	def comment_params
 		params.require(:comment).permit(:commenter, :body)
