900eb92eaeb74780e6314fca1e3a40be5e78e592
Nguyễn Thắng <nguyenthang@Nguyens-MacBook-Pro.local>
2016-05-15 00:56:33 +0700

    #2.1  create model Comment and create associating with Book

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160514091626) do
+ActiveRecord::Schema.define(version: 20160514175453) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -21,4 +21,15 @@ ActiveRecord::Schema.define(version: 20160514091626) do
     t.datetime "updated_at",               null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter",  limit: 255
+    t.text     "body",       limit: 65535
+    t.integer  "book_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "comments", ["book_id"], name: "index_comments_on_book_id", using: :btree
+
+  add_foreign_key "comments", "books"
 end
