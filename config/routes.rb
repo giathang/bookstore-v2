e9da3a237a3f5b0298d4dbb8db2c5370079fe252
Nguyễn Thắng <nguyenthang@Nguyens-MacBook-Pro.local>
2016-05-15 01:07:51 +0700

    #2.2 create controller Comment with create, destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'books#index'
-  resources :books
+  resources :books do
+    resources :comments
+  end
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
-      resources :books
+      resources :books do
+        resources :comments
+      end
     end
   end
 end
