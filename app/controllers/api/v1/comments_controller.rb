fea5e6408bc4c7951c8427d00a210a6a5784f393
Nguyễn Thắng <nguyenthang@Nguyens-MacBook-Pro.local>
2016-05-15 01:16:34 +0700

    #2.3 Create API Controller of function Comment

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,3 +1,28 @@
 class Api::V1::CommentsController < ApplicationController
+	before_action :find_book
 
+	# POST /api/v1/books/:id/comments
+	def create
+		unless @book.nil?
+			@comment = @book.comments.create(comment_params)
+		end
+	end
+
+	# DELETE /api/v1/books/:id/comments/:id
+	def destroy
+		unless @book.nil?
+			@comment = @book.comments.find(params[:id])
+			@comment.destroy
+		end
+	end
+
+	private
+
+	def find_book
+		@book = Book.find(params[:book_id])
+	end
+
+	def comment_params
+		params.require(:comment).permit(:commenter, :body)
+	end
 end
