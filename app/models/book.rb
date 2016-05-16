5146d65697e977e8c8a91306a60d4e0133ebfee3
GiaThang <githang.huflit@gmail.com>
2016-05-16 08:41:16 +0700

    #6.1-add-validates: Add validates for book, author, comment, category

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,7 @@ class Book < ActiveRecord::Base
 	has_many :assignments
 	has_many :authors, through: :assignments
 	belongs_to :category
+
+  validates_presence_of :title, :isbn, :des, :category_id
+  validates :isbn, length: {maximum: 5}
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :book
+  validates_presence_of :commenter, :body
 end
