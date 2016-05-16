7da03b8babfeb11c12e3b81f21b9c0a8a9b116ac
GiaThang <githang.huflit@gmail.com>
2016-05-16 10:12:13 +0700

    create new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,20 @@
 class CategoriesController < ApplicationController
+
   def show
     @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(category_params)
+    redirect_to new_book_path
+  end
+  private
+
+  def category_params
+    params.require(:category).permit(:category_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'books#index'
   resources :books
-  resources :categories, only: :show
-
+  resources :categories
 end
