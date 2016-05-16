d19cc6e1baa32b92cea42d8beb7294b7ce7c3214
GiaThang <githang.huflit@gmail.com>
2016-05-16 09:44:24 +0700

    update database.yml and secrets.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
   root 'books#index'
-  resources :books do
-    resources :comments
-  end
-  resources :authors
+  resources :books
   resources :categories, only: :show
 
-  #API
-  namespace :api, defaults: {format: 'json'} do
-    namespace :v1 do
-      resources :books do
-        resources :comments
-      end
-      resources :authors
-      resources :categories
-    end
-  end
 end
