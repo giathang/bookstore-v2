3e51bc231b50ad26f8e80882db7537934d8a67e4
GiaThang <githang.huflit@gmail.com>
2016-05-18 08:06:14 +0700

    update breadcumb

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -5,3 +5,12 @@ crumb :category do |category|
   link "Category / #{category.category_name}", category
   parent :root
 end
+crumb :book do |book|
+  link "Category / #{book.category.category_name} / #{book.title}", book
+  parent :root
+end
+
+crumb :no_category_book do |book|
+  link "#{book.title}", book
+  parent :root
+end
