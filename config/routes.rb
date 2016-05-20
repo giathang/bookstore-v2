96e3a5d8fe880752f10a1995e04db62d31e4e82c
GiaThang <githang.huflit@gmail.com>
2016-05-20 08:51:18 +0700

    create routes for API

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   root 'books#index'
   resources :books
   resources :categories
+
+  # create API
+  namespace :api, defauls: {format: 'json'} do
+    namespace :v1 do
+      resources :books
+      resources :categories
+    end
+  end
 end
