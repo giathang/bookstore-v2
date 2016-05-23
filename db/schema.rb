c7b5eed402b961d7949a5a04b079bd616c28d931
GiaThang <githang.huflit@gmail.com>
2016-05-23 09:36:10 +0700

    create model Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516030111) do
+ActiveRecord::Schema.define(version: 20160523023349) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20160516030111) do
     t.string   "category_name", limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_foreign_key "books", "categories"
