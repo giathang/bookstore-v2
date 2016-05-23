4d11caa92ee4c67666a8866382c29f507c8a6509
GiaThang <githang.huflit@gmail.com>
2016-05-23 09:41:00 +0700

    add validates to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email,
+            presence: true,
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: true
 end
