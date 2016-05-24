8c6f15a1bd0aac0580c636237b755592638c1026
GiaThang <githang.huflit@gmail.com>
2016-05-24 14:20:42 +0700

    function generate_token to auth_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,14 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: {minimum: 6}
   validates :password_confirmation, presence: true
-
+  before_create {generate_token(:auth_token)}
   def admin?
     self.role == 'admin'
+  end
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
   end
 end
