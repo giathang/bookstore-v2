28c7c720d4c3da57a2fe64edfff81011a91cb30c
GiaThang <githang.huflit@gmail.com>
2016-05-24 14:22:40 +0700

    fix sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :get_categories
   before_action :require_admin, except: [:new, :create]
+  before_action :get_categories
+
 
   def index
     @users = User.where(role: nil)
@@ -13,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      cookies[:auth_token] = @user.auth_token
       redirect_to root_path
     else
       render 'new'
